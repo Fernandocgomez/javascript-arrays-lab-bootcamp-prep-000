27344abe8e655bd8dac710acce7dd31eab740ed0
Fernando Cristobal De La Cruz <fernandocgomez@live.com>
2019-08-20 20:09:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
-  var newKittens = kittens.unshift()
+  var newKittens = kittens.unshift('Broom')
   return newKittens
 }
 
