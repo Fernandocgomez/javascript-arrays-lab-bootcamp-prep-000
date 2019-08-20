03b1866604e6b0dfe6fed513474eccd78e8b265c
Fernando Cristobal De La Cruz <fernandocgomez@live.com>
2019-08-20 19:52:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 // Add your functions and code here
 
 
-functions 
+function destructivelyAppendKitten(name) {
+  kittens.push()
+  return kittens
+}
